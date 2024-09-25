7fe52d177d6a81c1a98a9258250dea583397b1f5
Fabian Diaz <fabiandiaz@Fabians-MacBook-Pro.local>
2024-09-24 21:49:10 -0300

    add variables

--- hello_cargo/target/debug/hello_cargo.d
+++ hello_cargo/target/debug/hello_cargo.d
@@ -1,1 +1,1 @@
-/Users/fabian/rust-projects/hello_cargo/target/debug/hello_cargo: /Users/fabian/rust-projects/hello_cargo/src/main.rs
+/Users/fabiandiaz/github-repos/rust-practice-projects/hello_cargo/target/debug/hello_cargo: /Users/fabiandiaz/github-repos/rust-practice-projects/hello_cargo/src/main.rs
