b9f9d2f1eb2d76a6d2cbd2d469d567d4067b02d0
Fabian Diaz <diazfabian99@icloud.com>
2024-09-26 23:59:23 -0300

    add section 3 and statements and expresions

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,5 +1,22 @@
-fn main() {
-    let _x = 2.0; // f64
+use std::io;
 
-    let _y: f32 = 3.0; // f32
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
