97e85eae271057481d63ff1e13757b861333465b
Fabian Diaz <diazfabian99@icloud.com>
2024-09-29 23:58:01 -0300

    add manage memory cases

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,22 +1,29 @@
-use std::io;
-
 fn main() {
-    let a = [1, 2, 3, 4, 5];
+    let s1 = gives_ownership();         // gives_ownership moves its return
+                                        // value into s1
 
-    println!("Please enter an array index.");
+    let s2 = String::from("hello");     // s2 comes into scope
 
-    let mut index = String::new();
+    let s3 = takes_and_gives_back(s2);  // s2 is moved into
+                                        // takes_and_gives_back, which also
+                                        // moves its return value into s3
+} // Here, s3 goes out of scope and is dropped. s2 was moved, so nothing
+  // happens. s1 goes out of scope and is dropped.
 
-    io::stdin()
-        .read_line(&mut index)
-        .expect("Failed to read line");
+fn gives_ownership() -> String {             // gives_ownership will move its
+                                             // return value into the function
+                                             // that calls it
 
-    let index: usize = index
-        .trim()
-        .parse()
-        .expect("Index entered was not a number");
+    let some_string = String::from("yours"); // some_string comes into scope
 
-    let element = a[index];
+    some_string                              // some_string is returned and
+                                             // moves out to the calling
+                                             // function
+}
 
-    println!("The value of the element at index {index} is: {element}");
+// This function takes a String and returns one
+fn takes_and_gives_back(a_string: String) -> String { // a_string comes into
+                                                      // scope
+
+    a_string  // a_string is returned and moves out to the calling function
 }
