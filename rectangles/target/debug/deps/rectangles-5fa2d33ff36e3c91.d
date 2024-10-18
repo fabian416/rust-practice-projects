48ff963aae71b2abfa1ab3e3d19be7a04db6cb24
fabiandiaz416 <diazfabian99@icloud.com>
2024-10-17 23:53:26 -0300

    add example structures

---
+++ rectangles/target/debug/deps/rectangles-5fa2d33ff36e3c91.d
@@ -0,0 +1,5 @@
+/Users/fabiandiaz/personal-repos/rust-practice-projects/rectangles/target/debug/deps/rectangles-5fa2d33ff36e3c91: src/main.rs
+
+/Users/fabiandiaz/personal-repos/rust-practice-projects/rectangles/target/debug/deps/rectangles-5fa2d33ff36e3c91.d: src/main.rs
+
+src/main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/Users/fabiandiaz/personal-repos/rust-practice-projects/rectangles/target/debug/rectangles: /Users/fabiandiaz/personal-repos/rust-practice-projects/rectangles/src/main.rs
