4657ae0797cb76ca493ba61af54891de5b8a2643
fabiandiaz416 <diazfabian99@icloud.com>
2025-01-04 19:52:30 -0300

    add variable.rs

---
+++ hello_rust/src/bin/variable.rs
@@ -0,0 +1,20 @@
+#[allow(unused)]
+
+fn main() {
+    let x: i32 = -123;
+
+    // x += 4;
+
+    let mut y = 24;
+    y += 1;
+
+    // default type is signed integer i32
+    let z = -123;
+    const NUM: u32 = 1;
+
+    // Re declare a variable is called shadowing
+    let x: i32 = -1;
+    let x: bool = true;
+
+    let x: Vec<_> = vec![1, 2, 3];
+}
