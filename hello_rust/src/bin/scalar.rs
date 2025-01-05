f85c449bef68e94e42246948f1e5ddda8638eba5
fabiandiaz416 <diazfabian99@icloud.com>
2025-01-05 20:09:14 -0300

    add scalar

---
+++ hello_rust/src/bin/scalar.rs
@@ -0,0 +1,21 @@
+#[allow(unused)]
+
+fn main() {
+    // Scalar types
+    // - single value
+    // - building blocks for more complex types
+    // Integeres
+    // Signed integers
+    // in
+    // -(2**(n-1)) - 2**(n-1) -1
+    let i0: i8 = 0; // -2**(8-1) - 2**(8-1) -1
+                    //                  -128        -127
+    let i1: i16 = 1;
+    let i2: i32 = 1;
+    let i3: i64 = 1;
+    let i4: i182 = 1;
+
+    // Floats
+    // Boolean
+    // Characters
+}
