21047db71aa316d5fa28d273fcea317ff016be22
fabiandiaz416 <diazfabian99@icloud.com>
2025-01-06 16:09:59 -0300

    array examples with slices too

---
+++ hello_rust/src/bin/array.rs
@@ -0,0 +1,21 @@
+#[allow(unused)]
+
+fn main() {
+    // Array - fixed length, known at compile time
+    let arr: [u32; 3] = [1, 2, 3];
+    print!("arr[1] = {} ", arr[1]);
+
+    let mut arr: [u32; 3] = [1, 2, 3];
+    arr[1] = 9;
+    print!("arr[1] now is = {} ", arr[1]);
+
+    let arr: [u32; 10] = [0; 10];
+    print!("{:?}", arr);
+
+    // Slices
+    let nums: [i32; 10] = [-1, 1, -2, 2, -3, 3, -4, 4, -5, 5];
+
+    // First 3 elements
+    let s = &nums[0..3]; // not included the third element so you get: 0, 1, 2
+    print!("First 3 elements are: {:?}", s);
+}
